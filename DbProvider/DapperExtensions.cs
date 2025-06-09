2801124fd856a262c2345f69d235a1cee7f98d47
MichaelSkirda <85059703+MichaelSkirda@users.noreply.github.com>
2025-06-09 15:56:43 +0300

    refactor: class rename

--- DbProvider/DapperExtensions.cs
+++ DbProvider/DapperExtensions.cs
@@ -5,9 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace DbProvider
+namespace DatabaseProvider
 {
-	public static class DupperExtensions
+	public static class DapperExtensions
 	{
 		private static bool _switchToNewConnectino = true;
 
